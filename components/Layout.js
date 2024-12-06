0259fc397e2dd74b6c9cbec7bffbe7ecb709bc72
fuglsetm <fuglsetm@gmail.com>
2024-12-06 12:35:49 +0000

    animation + name

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,36 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import Header from './Header';
 import Footer from './Footer';
 import ContactForm from './ContactForm';
+
+const pageTransition = {
+  initial: { opacity: 0, y: 10 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -10 },
+};
 
 const Layout = ({ children }) => (
   <div className="flex flex-col min-h-screen bg-white text-black font-serif">
     {/* Header */}
     <Header />
 
-    {/* Main Content */}
+    {/* Main Content with Page Transitions */}
     <main className="flex-grow">
-      <div className="container mx-auto px-4">
-        {children}
-      </div>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={children.key || 'default'}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={pageTransition}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="container mx-auto px-4">{children}</div>
+        </motion.div>
+      </AnimatePresence>
     </main>
 
-    {/* Kontakt */}
+    {/* Contact Form */}
     <div className="container mx-auto px-4">
       <ContactForm />
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
-
+import { motion, AnimatePresence } from 'framer-motion';
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }) {
+const pageTransition = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -20 },
+};
+
+export default function MyApp({ Component, pageProps, router }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Anne Marie</title>
+        <meta name="description" content="Welcome to Anne Marie's website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.div
+            key={router.route}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={pageTransition}
+            transition={{ duration: 0.5 }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </>
   );
 }
